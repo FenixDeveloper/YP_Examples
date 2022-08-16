c34880e5a316711e939534bf22c18925d245604d
fenix <fenixdeveloper@gmail.com>
2022-08-16 12:07:44 +0300

    webinar: js 2

---
+++ js-dynamic/script.js
@@ -0,0 +1,82 @@
+//debugger;
+
+const $selectMethod = document.getElementById('method');
+const $buttonAdd = document.getElementById('add-photo');
+const $photos = document.querySelector('.photos');
+
+const generatePhoto = index => ([`https://placehold.co/250?text=${index}`, `Image ${index}`]);
+
+function clonePhoto($div, $img, $p) {
+    return (image, title) => {
+        //debugger;
+        $img.src = image;
+        $img.alt = title;
+        $p.textContent = title;
+        const $target = $div.cloneNode(true);
+        const $close = $target.querySelector('button');
+        $close.addEventListener('click', (e) => {
+            e.stopPropagation();
+            $target.remove();
+        });
+        return $target;
+    };
+}
+
+function initCreatedPhoto() {
+    const $div = document.createElement('div');
+    const $img = document.createElement('img');
+    const $p = document.createElement('p');
+    const $btn = document.createElement('button');
+    $btn.innerHTML = '<i class="icon-trash-can-solid"></i>';
+
+    /*
+    $div.innerHTML = '<img src="" alt="" /><p></p>';
+    const $img = $div.querySelector('img');
+    const $p = $div.querySelector('p');
+    */
+
+    $img.src = 'https://placehold.co/250?text=EMPTY';
+    $img.alt = '';
+    $p.textContent = '';
+
+    $div.append($btn, $img, $p);
+
+    return clonePhoto($div, $img, $p);
+}
+
+function initFirstPhoto($container) {
+    const $div = $container.querySelector('div');
+    const $img = $div.querySelector('img');
+    const $p = $div.querySelector('p');
+
+    $div.remove();
+
+    return clonePhoto($div, $img, $p);
+}
+
+function initTemplatePhoto(id) {
+    const $template = document.getElementById(id);
+    const $inner = document.createElement('div');
+    $inner.innerHTML = $template.innerText;
+
+    const $div = $inner.querySelector('*:first-child');
+    const $img = $div.querySelector('img');
+    const $p = $div.querySelector('p');
+
+    return clonePhoto($div, $img, $p);
+}
+
+const createMethods = {
+    create: initCreatedPhoto(),
+    first: initFirstPhoto($photos),
+    template: initTemplatePhoto('photo-template')
+};
+
+$buttonAdd.addEventListener('click', () => {
+    //debugger;
+    $photos.append(
+        createMethods[$selectMethod.value](
+            ...generatePhoto($photos.childElementCount)
+        )
+    );
+});
