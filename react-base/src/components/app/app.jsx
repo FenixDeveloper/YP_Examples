e277a014578d31ee2bae94a37a7817ec3ef8b319
fenix <fenixdeveloper@gmail.com>
2022-12-08 16:51:55 +0300

    react base: step 1

--- react-base/src/components/app/app.jsx
+++ react-base/src/components/app/app.jsx
@@ -1,8 +1,23 @@
+import React from 'react';
 import styles from './app.module.css';
+
+class ButtonsGroup extends React.Component {
+    render() {
+        return <div className="buttons">
+            {this.props.children}
+        </div>
+    }
+}
+
+function Button({label, ...props}) {
+    return <button {...props}>{label}</button>
+}
 
 function App() {
     return <div className={styles.app}>
-        Hello world!
+        <ButtonsGroup>
+            <Button label="CLICK ME!" onClick={() => alert('Hello world!')} />
+        </ButtonsGroup>
     </div>
 }
 
